66f7c5c53ed7781f8d6ce4dc818a6ccafe3e2638
akrm19 <akrm19@gmail.com>
2018-04-07 01:31:33 -0500

    Move Sunlight svcs to Sunlight api folder

---
+++ Write2Congress.Shared/DomainModel/ApiModels/Sunlight/SunlightBaseResult.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Write2Congress.Shared.DomainModel
+{
+    public abstract class SunlightBaseResult
+    {
+        public class BaseRootObject
+        {
+            //public object[] results { get; set; }
+            public int? count { get; set; }
+            public Page page { get; set; }
+        }
+
+        public class Page
+        {
+            public int? count { get; set; }
+            public int? per_page { get; set; }
+            public int? page { get; set; }
+        }
+    }
+}
