67f1a8471fb07bb96150c0b4405110c1997d0328
akrm19 <akrm19@gmail.com>
2018-08-30 21:25:28 -0500

    commit changes to resource designer

--- Write2Congress/Write2Congress.Droid/Resources/Resource.designer.cs
+++ Write2Congress/Write2Congress.Droid/Resources/Resource.designer.cs
@@ -2437,26 +2437,26 @@ namespace Write2Congress.Droid
 			// aapt resource value: 0x7f020053
 			public const int avd_hide_password = 2130837587;
 			
-			// aapt resource value: 0x7f0200aa
-			public const int avd_hide_password_1 = 2130837674;
-			
-			// aapt resource value: 0x7f0200ab
-			public const int avd_hide_password_2 = 2130837675;
-			
 			// aapt resource value: 0x7f0200ac
-			public const int avd_hide_password_3 = 2130837676;
+			public const int avd_hide_password_1 = 2130837676;
+			
+			// aapt resource value: 0x7f0200ad
+			public const int avd_hide_password_2 = 2130837677;
+			
+			// aapt resource value: 0x7f0200ae
+			public const int avd_hide_password_3 = 2130837678;
 			
 			// aapt resource value: 0x7f020054
 			public const int avd_show_password = 2130837588;
 			
-			// aapt resource value: 0x7f0200ad
-			public const int avd_show_password_1 = 2130837677;
-			
-			// aapt resource value: 0x7f0200ae
-			public const int avd_show_password_2 = 2130837678;
-			
 			// aapt resource value: 0x7f0200af
-			public const int avd_show_password_3 = 2130837679;
+			public const int avd_show_password_1 = 2130837679;
+			
+			// aapt resource value: 0x7f0200b0
+			public const int avd_show_password_2 = 2130837680;
+			
+			// aapt resource value: 0x7f0200b1
+			public const int avd_show_password_3 = 2130837681;
 			
 			// aapt resource value: 0x7f020055
 			public const int common_full_open_on_phone = 2130837589;
@@ -2660,58 +2660,64 @@ namespace Write2Congress.Droid
 			public const int ic_star_rate_blue_48dp = 2130837655;
 			
 			// aapt resource value: 0x7f020098
-			public const int ic_supervisor_account_white_48dp = 2130837656;
+			public const int ic_star_rate_border_48dp = 2130837656;
 			
 			// aapt resource value: 0x7f020099
-			public const int ic_thumb_down_white_48dp = 2130837657;
+			public const int ic_supervisor_account_white_48dp = 2130837657;
 			
 			// aapt resource value: 0x7f02009a
-			public const int ic_thumb_up_white_48dp = 2130837658;
+			public const int ic_thumb_down_white_48dp = 2130837658;
 			
 			// aapt resource value: 0x7f02009b
-			public const int Icon = 2130837659;
+			public const int ic_thumb_up_white_48dp = 2130837659;
 			
 			// aapt resource value: 0x7f02009c
-			public const int navigation_empty_icon = 2130837660;
+			public const int ic_wrench_blue_48dp = 2130837660;
 			
 			// aapt resource value: 0x7f02009d
-			public const int notification_action_background = 2130837661;
+			public const int Icon = 2130837661;
 			
 			// aapt resource value: 0x7f02009e
-			public const int notification_bg = 2130837662;
+			public const int navigation_empty_icon = 2130837662;
 			
 			// aapt resource value: 0x7f02009f
-			public const int notification_bg_low = 2130837663;
+			public const int notification_action_background = 2130837663;
 			
 			// aapt resource value: 0x7f0200a0
-			public const int notification_bg_low_normal = 2130837664;
+			public const int notification_bg = 2130837664;
 			
 			// aapt resource value: 0x7f0200a1
-			public const int notification_bg_low_pressed = 2130837665;
+			public const int notification_bg_low = 2130837665;
 			
 			// aapt resource value: 0x7f0200a2
-			public const int notification_bg_normal = 2130837666;
+			public const int notification_bg_low_normal = 2130837666;
 			
 			// aapt resource value: 0x7f0200a3
-			public const int notification_bg_normal_pressed = 2130837667;
+			public const int notification_bg_low_pressed = 2130837667;
 			
 			// aapt resource value: 0x7f0200a4
-			public const int notification_icon_background = 2130837668;
-			
-			// aapt resource value: 0x7f0200a8
-			public const int notification_template_icon_bg = 2130837672;
-			
-			// aapt resource value: 0x7f0200a9
-			public const int notification_template_icon_low_bg = 2130837673;
+			public const int notification_bg_normal = 2130837668;
 			
 			// aapt resource value: 0x7f0200a5
-			public const int notification_tile_bg = 2130837669;
+			public const int notification_bg_normal_pressed = 2130837669;
 			
 			// aapt resource value: 0x7f0200a6
-			public const int notify_panel_notification_icon_bg = 2130837670;
+			public const int notification_icon_background = 2130837670;
+			
+			// aapt resource value: 0x7f0200aa
+			public const int notification_template_icon_bg = 2130837674;
+			
+			// aapt resource value: 0x7f0200ab
+			public const int notification_template_icon_low_bg = 2130837675;
 			
 			// aapt resource value: 0x7f0200a7
-			public const int preference_list_divider_material = 2130837671;
+			public const int notification_tile_bg = 2130837671;
+			
+			// aapt resource value: 0x7f0200a8
+			public const int notify_panel_notification_icon_bg = 2130837672;
+			
+			// aapt resource value: 0x7f0200a9
+			public const int preference_list_divider_material = 2130837673;
 			
 			static Drawable()
 			{
