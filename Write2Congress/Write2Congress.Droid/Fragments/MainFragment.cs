6030597ebdac67b5244c25999017139aa083da44
akrm19 <akrm19@gmail.com>
2018-09-18 02:39:22 -0500

    Retain fragment in main activity

--- Write2Congress/Write2Congress.Droid/Fragments/MainFragment.cs
+++ Write2Congress/Write2Congress.Droid/Fragments/MainFragment.cs
@@ -34,7 +34,8 @@ namespace Write2Congress.Droid.Fragments
         public override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
-            // Create your fragment here
+
+            RetainInstance = true;
         }
 
         public override View OnCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
